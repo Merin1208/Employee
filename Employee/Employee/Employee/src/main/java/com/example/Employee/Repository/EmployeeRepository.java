583b508c86ea9a72dc2c195f56a091301ed948ef
Merin1208 <130655465+Merin1208@users.noreply.github.com>
2025-02-17 04:04:17 +0530

    Delete Employee/Employee/Employee directory

--- Employee/Employee/Employee/src/main/java/com/example/Employee/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Employee.Repository;
-
-import com.example.Employee.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
